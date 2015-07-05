91c728f016a3c22f99fc3740741d5fb8da6712e4
valdrinkoshi <valdrinkoshi@gmail.com>
2015-07-05 16:25:05 -0700

    new folder structure

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,12 +7,39 @@ module.exports = function(grunt) {
         globals: {}
       }
     },
-    watch: {}
+    watch: {},
+    copy: {
+      client: {
+        expand: true,
+        cwd: 'client',
+        dest: 'parse/public/',
+        src: '**/*'
+      },
+      server: {
+        expand: true,
+        cwd: 'server',
+        dest: 'parse/cloud/',
+        src: '**/*'
+      }
+    },
+    clean: {
+      parse: {
+        files: [{
+          dot: true,
+          src: [
+            'parse/cloud/*',
+            'parse/public/*',
+          ]
+        }]
+      }
+    },
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  //grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-copy');
 
   grunt.registerTask('default', ['jshint']);
-
+  grunt.registerTask('serve', ['clean:parse','copy:client','copy:server']);
 };

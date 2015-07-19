93ecbba6a8cefbd839ae0e86c5841f617de64df5
valdrinkoshi <valdrinkoshi@gmail.com>
2015-07-19 02:27:24 -0700

    check for duplicates. Expose only Parse Cloud job

--- server/jobs.js
+++ server/jobs.js
@@ -5,7 +5,7 @@ var restrictedAcl = new Parse.ACL();
 restrictedAcl.setPublicReadAccess(false);
 restrictedAcl.setPublicWriteAccess(false);
 
-var Job = Parse.Object.extend("Job");
+var Job = Parse.Object.extend('Job');
 
 // TODO load more jobs from different APIs
 function getJobs() {
@@ -20,37 +20,72 @@ function getJobs() {
             'User-Agent': 'Parse.com Cloud Code'
         }
     };
-    return Parse.Cloud.httpRequest(data);
+    return Parse.Cloud.httpRequest(data).then(function (response) {
+        return response.data;
+    });
 }
-function getEmails(text) {
-    return text.match(/([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+)/gi).join('; ');
+
+function getContact(rawData) {
+    var text = JSON.stringify(rawData);
+    var emails = text.match(/([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+)/gi) || [];
+    return _.uniq(emails, false, function (email) {
+        return email.toLowerCase();
+    }).join('; ');
 }
 
 function saveJob(jobData) {
-    var contact = getEmails(JSON.stringify(jobData));
-    if (contact && contact.length) {
-        var job = new Job();
-        job.setACL(restrictedAcl);
-        job.contact = contact;
-        job.data = jobData;
-        return job.save(null, {useMasterKey: true});
-    }
+    var contact = getContact(jobData);
+    var job = new Job();
+    job.setACL(restrictedAcl);
+    return job.save({
+        contact: contact,
+        rawData: jobData
+    }, {useMasterKey: true});
 }
 
 function saveJobs(jobs) {
-    var jobsPromises = _.map(jobs, saveJob);
-    _.remove(jobsPromises, _.isUndefined);
-    return jobsPromises;
+    var promises = _.map(jobs, saveJob);
+    return Parse.Promise.when(promises);
 }
 
-Parse.Cloud.job('getJobs', function (request, status) {
-    // Set up to modify user data
-    Parse.Cloud.useMasterKey();
-    getJobs().then(saveJobs).then(function () {
-        // Set the job's success status
-        status.success("Migration completed successfully.");
-    }, function (error) {
-        // Set the job's error status
-        status.error("Uh oh, something went wrong.");
+/**
+ * @name isNewJob
+ * @param job
+ * @returns {Promise} always resolved, pass job object only if new
+ */
+function isNewJob(job) {
+    var query = new Parse.Query(Job);
+    // TODO use better way to see if is the same job, e.g. build hashKey
+    query.equalTo('rawData', job);
+    return query.first({useMasterKey: true})
+        .then(function (jobObj) {
+            if (jobObj) {
+                job = undefined;
+            }
+        })
+        .always(function () {
+            return Parse.Promise.as(job);
+        });
+}
+
+function filterJobs(jobs) {
+    console.log('filterJobs: jobs ' + jobs.length);
+    var promises = _.map(jobs, isNewJob);
+    return Parse.Promise.when(promises).then(function () {
+        var newJobs = _.filter(arguments, _.isObject);
+        console.log('filterJobs: newJobs ' + newJobs.length);
+        return newJobs;
     });
+}
+
+Parse.Cloud.job('loadJobs', function (request, status) {
+    Parse.Cloud.useMasterKey();
+    getJobs()
+        .then(filterJobs)
+        .then(saveJobs)
+        .then(function () {
+            status.success('Saved successfully ' + arguments.length + ' new jobs');
+        }, function (error) {
+            status.error('Failed to save new jobs. ' + error);
+        });
 });

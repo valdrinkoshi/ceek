c07d6fca4da15db1e1801f468146ca9fb2f05d5b
ventuno <ernestom@feynman.local>
2015-08-04 18:34:53 -0700

    Improving user table to display details with actions.

--- client/scripts/admin/filterabletable.js
+++ client/scripts/admin/filterabletable.js
@@ -1,15 +1,31 @@
 var React = require('react');
+var ReactBootstrap = require('react-bootstrap');
+var DropdownButton = ReactBootstrap.DropdownButton;
+var MenuItem = ReactBootstrap.MenuItem;
 
 var TableRow = React.createClass({
+  _handleActionSelect(event) {
+    this.props.onSelectAction(event, this.props.rowData);
+  },
+
   render() {
     var data = this.props.rowData;
     var columns = this.props.columns || Object.keys(this.props.rowData);
+    var actions = this.props.actions || [];
     var tableColumns = columns.map(function (column, index) {
       return (<td key={index}>{data[column]}</td>);
+    });
+    var actionsMenuItems = actions.map(function (action, index) {
+      return (<MenuItem key={action.eventKey} eventKey={action.eventKey}>{action.name}</MenuItem>);
     });
     return (
       <tr>
         {tableColumns}
+        <td>
+          <DropdownButton onSelect={this._handleActionSelect} title='Actions' key='actions'>
+            {actionsMenuItems}
+          </DropdownButton>
+        </td>
       </tr>
     );
   }
@@ -34,17 +50,20 @@ var FilterableTable = React.createClass({
     var filters = {};
     for (var filterName in this.refs) {
       var filterValue = React.findDOMNode(this.refs[filterName]).value.trim();
-      filters[filterName] = filterValue;
+      if (filterValue) {
+        filters[filterName] = filterValue;
+      }
     }
     this.props.onFilter(filters);
     return;
   },
 
   render() {
+    var _this = this;
     var columns = this.props.columns;
     var tableRows = this.props.data.map(function (rowData, index) {
       return (
-        <TableRow key={index} rowData={rowData} columns={columns} />
+        <TableRow actions={_this.props.actions} key={index} rowData={rowData} columns={columns} onSelectAction={_this.props.onSelectAction} />
       );
     });
 
--- client/scripts/admin/userstable.js
+++ client/scripts/admin/userstable.js
@@ -1,11 +1,40 @@
 var React = require('react');
-var ReactBootstrap = require('react-bootstrap');
 var FilterableTable = require('FilterableTable');
 var AdminServices = require('../AdminServices.js');
+var ReactBootstrap = require('react-bootstrap');
+var Button = ReactBootstrap.Button;
+var Modal = ReactBootstrap.Modal;
+
+var UserDetailModal = React.createClass({
+  getInitialState() {
+    return {
+      show: false
+    };
+  },
+
+  render() {
+    if (!this.props.data) {
+      return null;
+    }
+    return (
+      <div>
+        <Modal show={this.props.show} onHide={this.props.onHide}>
+          <Modal.Header closeButton>
+            <Modal.Title>User Details</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <p>{this.props.data.firstName}</p>
+          </Modal.Body>
+        </Modal>
+      </div>
+    );
+  }
+});
 
 var UsersTable = React.createClass({
   getInitialState() {
     return {
+      showUserDetails: false,
       data: [],
       columns: [],
       filters: []
@@ -20,9 +49,10 @@ var UsersTable = React.createClass({
     var _this = this;
     AdminServices.GetUsers(filters).then(function (data) {
       _this.setState({
+        showUserDetails: false,
         data: data,
         columns: ['firstName', 'lastName'],
-        filters: ['firstName']
+        filters: ['firstName', 'emailAddress']
       });
     });
   },
@@ -32,12 +62,29 @@ var UsersTable = React.createClass({
     return;
   },
 
+  _openUserDetails(userData) {
+    this.setState({
+      showUserDetails: true,
+      userData: userData
+    });
+  },
+
+  _closeUserDetails() {
+    this.setState({
+      showUserDetails: false
+    });
+  },
+
+  _handleSelectAction(event, userData) {
+    this._openUserDetails(userData);
+  },
+
   render() {
-    
+    var actions = [{name: 'View Details', eventKey: '1'}]
     return (
       <div>
-        
-        <FilterableTable data={this.state.data} onFilter={this._handleFilter} columns={this.state.columns} filters={this.state.filters} />
+        <FilterableTable data={this.state.data} onFilter={this._handleFilter} columns={this.state.columns} filters={this.state.filters} onSelectAction={this._handleSelectAction} actions={actions} />
+        <UserDetailModal data={this.state.userData} show={this.state.showUserDetails} onHide={this._closeUserDetails} />
       </div>
     );
   }

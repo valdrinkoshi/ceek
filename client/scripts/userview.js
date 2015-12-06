cf782b8a6a8ab356eb929074b3a79c92d3810cdf
ventuno <ernestom@feynman.local>
2015-12-06 15:27:11 -0800

    Additional styling to profile view and work style chart.

--- client/scripts/userview.js
+++ client/scripts/userview.js
@@ -3,12 +3,14 @@ var ReactRouter = require('react-router');
 var Services = require('./Services.js');
 var UserProfileHeader = require('./UserProfileHeader.js');
 var classNames = require('classnames');
+var Chartjs = require('react-chartjs');
+var Doughnut = Chartjs.Doughnut;
 
 var ProfileSection = React.createClass({
   render () {
     return (
-      <div>
-        <h4>{this.props.title}</h4>
+      <div className='profile-view-section'>
+        <h4 className='profile-view-section-title'>{this.props.title}</h4>
         {this.props.children}
       </div>
     );
@@ -59,7 +61,7 @@ var ProfileProperty = React.createClass({
     }
     
     return (
-      <div>
+      <div className='profile-property'>
         <label className={classNames(labelClasses)}>{this.props.name}:</label>
         <div className={classNames(valueClasses)}>
           {children}
@@ -105,10 +107,6 @@ var HistoryProfileProperty = React.createClass({
   }
 });
 
-/*var ListProfileProperty = React.createClass({
-
-});*/
-
 var UserView = React.createClass({
   mixins: [ReactRouter.State, ReactRouter.Navigation],
 
@@ -129,6 +127,122 @@ var UserView = React.createClass({
       });
     } else {
       this.transitionTo("/");
+    }
+  },
+
+  computeWorkStyle(data) {
+    var questionnaire;
+    var totQuestions = 8;
+    var questionPropertyNameBase = 'questionnaire_question';
+    //default profile types
+    var profileTypes = {
+        '1': {
+          value: 0,
+          color: '#72D0F4',
+          label: 'Driver',
+          description: "Drivers are people who challenge the team to improve. They are dynamic and usually extroverted people who enjoy stimulating others for solving problems. They guide the team to what they perceive are the objectives. They are efficient, practical, and systematic."
+        },
+        '2': {
+          value: 0,
+          color: '#F5CE69',
+          label: 'Implementer',
+          description: "Implementers are the people who get things done. They turn the team's ideas and concepts into practical actions and plans. They are typically conservative, disciplined people who work systematically and efficiently and are very well organized."
+        },
+        '3': {
+          value: 0,
+          color: '#F7929C',
+          label: 'Innovator',
+          description: 'The innovators are often the creative generators of a team. They are investigative, interested and curious about things. The innovator prefers to be independent and tends to approach tasks in a scientific way. As a creative individual the innovator may play a crucial role in the way a team approaches tasks and solves problems.'
+        },
+        '4': {
+          value: 0,
+          color: '#9393F5',
+          label: 'Investigator',
+          description: "Investigators have a tendency to be reserved and critical. They will react to plans and ideas in a rational and sensible way. They favor a prudent approach to matters and will evaluate them according to their accuracy before acting"
+        }
+      };
+      var mappings = {
+        //questionnaire_question#
+        '1': {
+          //reply#->type#
+          '1': '1',
+          '2': '2',
+          '3': '3',
+          '4': '4'
+        },
+        '2': {
+          '1': '4',
+          '2': '2',
+          '3': '1',
+          '4': '3'
+        },
+        '3': {
+          '1': '1',
+          '2': '3',
+          '3': '2',
+          '4': '4'
+        },
+        '4': {
+          '1': '2',
+          '2': '4',
+          '3': '1',
+          '4': '2'
+        },
+        '5': {
+          '1': '2',
+          '2': '1',
+          '3': '3',
+          '4': '4'
+        },
+        '6': {
+          '1': '3',
+          '2': '1',
+          '3': '2',
+          '4': '4'
+        },
+        '7': {
+          '1': '4',
+          '2': '3',
+          '3': '1',
+          '4': '2'
+        },
+        '8': {
+          '1': '2',
+          '2': '4',
+          '3': '3',
+          '4': '1'
+        }
+      }
+    var matchingTypes = jQuery.extend(true, {}, profileTypes);
+    for (var i = 1; i < totQuestions+1; i++) {
+      var question = questionPropertyNameBase+i;
+      if (typeof data[question] === 'string') {
+        var typeForQuestion = mappings[i][data[question]];
+        matchingTypes[typeForQuestion].value += 1;
+      } else {
+        questionnaire = undefined;
+        break;
+      }
+    }
+
+    data.workStyleChartData = matchingTypes;
+    data.mainWorkStyleData = {};
+    var rankingTypes = Object.keys(profileTypes);
+    rankingTypes.sort(function (a, b) {
+      return matchingTypes[a].value - matchingTypes[b].value
+    }).reverse();
+    var maxScore = matchingTypes[rankingTypes[0]].value;
+    if (maxScore > 3) {
+      console.log('type', profileTypes[rankingTypes[0]].label)
+      data.mainWorkStyleData[rankingTypes[0]] = profileTypes[rankingTypes[0]];
+    } else if (maxScore == 3) {
+      var nextMaxScore = matchingTypes[rankingTypes[1]].value;
+      data.mainWorkStyleData[rankingTypes[0]] = profileTypes[rankingTypes[0]];
+      if (nextMaxScore === 3) {
+        data.mainWorkStyleData[rankingTypes[1]] = profileTypes[rankingTypes[1]];
+      }
+    } else {
+      data.mainWorkStyleData = matchingTypes;
     }
   },
 
@@ -163,6 +277,25 @@ var UserView = React.createClass({
           return <span key={i} className='boxed-property'>{contrib.process} {contrib.percentage}%</span>
         });
       }
+
+      this.computeWorkStyle(data);
+      var workStyleDescriptionBase = 'Based on the questionnaire result, ' + data.firstName;
+      var workStyleDescription = '';
+      var workStyleIds = Object.keys(data.mainWorkStyleData);
+      if (workStyleIds.length === 1) {
+        workStyleDescription = workStyleDescriptionBase + "'s  dominant work style is " + data.mainWorkStyleData[workStyleIds[0]].label;
+      } else if (workStyleIds.length === 2) {
+        workStyleDescription = workStyleDescriptionBase + "'s  dominant work style is " + data.mainWorkStyleData[workStyleIds[0]].label + ", and his supporting work style is " + data.mainWorkStyleData[workStyleIds[1]].label;
+      } else {
+        workStyleDescription = workStyleDescriptionBase + " has a perfectly balanced among all the Ceek working styles"
+      }
+
+      var workStylesDetailedDescription = [];
+      for (var workStyleId in data.mainWorkStyleData) {
+        var workStyle = data.mainWorkStyleData[workStyleId]
+        workStylesDetailedDescription.push(<span className='work-style-detail'>{workStyle.description}</span>)
+      }
+
       output =
         <div className='container'>
           <UserProfileHeader pictureUrl={data.pictureUrl} firstName={data.firstName} lastName={data.lastName} emailAddress={data.emailAddress} marketStatus={data.marketStatus} marketStatusText={data.marketStatusText} />
@@ -199,7 +332,18 @@ var UserView = React.createClass({
             </ProfileProperty>
           </ProfileSection>
           <ProfileSection title='Questionnaire Result'>
-            
+            <div className='row'>
+            <div className='col-xs-12 col-sm-12 col-md-4 col-lg-4 vcenter'>
+              <Doughnut data={data.workStyleChartData} />
+            </div>
+            <div className='work-style-desc-box col-xs-12 col-sm-12 col-md-8 col-lg-8 vcenter'>
+              <div className='work-style-what'>What's {data.firstName}'s work style?</div>
+              <div className='work-style-main'>{workStyleDescription}.</div>
+              <div>
+              {workStylesDetailedDescription}
+              </div>
+            </div>
+            </div>
           </ProfileSection>
         </div>;
     } else {
